00a9ba54c5a84dd3d32936cf128c2c25b8d33c33
Adam Golinski <adam.gol1993@gmail.com>
2017-08-09 11:25:57 +0100

    started working on loading data from R data files

--- src/ox/bugs2foppl/Main.java
+++ src/ox/bugs2foppl/Main.java
@@ -1,20 +1,114 @@
 package ox.bugs2foppl;
 
-import org.antlr.v4.misc.Graph;
-import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import ox.bugs2foppl.gen.*;
+import org.renjin.script.RenjinScriptEngineFactory;
+import org.renjin.sexp.AttributeMap;
+import org.renjin.sexp.Vector;
+import ox.bugs2foppl.gen.bugsLexer;
+import ox.bugs2foppl.gen.bugsParser;
 
-import javax.swing.*;
+import javax.script.ScriptEngine;
 import java.io.FileInputStream;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.FileReader;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args) throws Exception {
+
+        // read file
+        String inputFile = "examples/examples_JAGS/classic-bugs/vol1/equiv/equiv-data.R";
+
+        Scanner in = new Scanner(new FileReader(inputFile));
+
+        StringBuilder sb = new StringBuilder();
+        while(in.hasNext()) {
+            sb.append(' ');
+            sb.append(in.next());
+        }
+        in.close();
+
+        String data_file = sb.toString();
+
+        String pattern = "(\"(\\S+)\"\\s*<-\\s*(?:.|\\n)*?)(?=\"\\S+\"\\s*<-|\\z)";
+
+
+        System.out.println("input: " + data_file);
+
+        System.out.println("pattern: " + pattern);
+
+        Pattern p = Pattern.compile(pattern, Pattern.MULTILINE);
+        Matcher m = p.matcher(data_file);
+
+        List<String> Rcommands = new LinkedList<>();
+
+        Map<String, String> varToFOPPLString = new HashMap<>();
+
+        RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+        ScriptEngine engine = factory.getScriptEngine();
+
+        while (m.find()) {
+            String var_name = m.group(2);
+            String statement = m.group(1);
+
+            Vector res = (Vector) engine.eval(statement);
+
+            String objectTypeName = res.getClass().getName();
+
+            System.out.println("var_name : " + var_name);
+            System.out.println("expression: " + statement);
+            System.out.println("object type: " + objectTypeName);
+
+            String expectedType = "org.renjin.sexp.DoubleArrayVector";
+            if (!objectTypeName.equals(expectedType)) {
+                throw new Exception("Not expected type: " + expectedType);
+            } else {
+                varToFOPPLString.put(var_name,
+                        convertVectorToFOPPLString(res, var_name));
+            }
+        }
+    }
+
+    static String convertVectorToFOPPLString(Vector res, String name) throws Exception {
+        if (res.hasAttributes()) {
+            AttributeMap attributes = res.getAttributes();
+            Vector dim = attributes.getDim();
+            if (dim == null) {
+                return convertVectorToFOPPLStringCaseVector(res, name);
+            } else {
+
+            }
+        } else {
+            return convertVectorToFOPPLStringCaseVector(res, name);
+        }
+        return "";
+    }
+
+    static List convertVectorToMatrix(List v, List dim) throws Exception {
+
+    }
+
+    static String convertVectorToFOPPLStringCaseVector(Vector res, String name) throws Exception {
+        System.out.println("Result is a vector of length " + res.length());
+        if (res.length() == 1) {
+            return String.format("(def %s %s)", name, res.getElementAsDouble(0));
+        } else if (res.length() > 1) {
+            String numbers_string = "";
+            for (int i=0; i<res.length(); i++) {
+                numbers_string += res.getElementAsString(i);
+                numbers_string += " ";
+            }
+            return String.format("(def %s (vector %s))", name, numbers_string);
+        } else {
+            throw new Exception("res.length() < 1");
+        }
+    }
+
+    static void main2() throws Exception {
 
         String inputFile = "examples/PLA2_example3";
 
